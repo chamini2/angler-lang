cec6f5a5f10e565a21580ba0b0fdd25f325f4b75
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-08-22 23:53:48 -0430

    If we pass no arguments, the program uses the standard input; if we do pass an argument, the program uses the first one as the file to read input from.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,17 @@ module Main where
 
 import           Language.Angler.Parser.Lexer (runLexer)
 
+import           System.Environment           (getArgs)
+
+import           Language.Angler.SrcLoc (unlocate)
+import           Data.List              (intercalate)
+
 main :: IO ()
 main = do
-        input <- getContents
-        either print (mapM_ print) (runLexer input)
+        args <- getArgs
+        input <- case args of
+                f : _ -> readFile f
+                _     -> getContents
+        case runLexer input of
+                Right ltks -> putStrLn . intercalate " " $ map (show . unlocate) ltks
+                Left  err  -> print err
