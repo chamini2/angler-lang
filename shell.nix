3f301cbb4d437de804442b0e2e93913a88334d74
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-09-08 16:23:59 -0430

    Added utf8-string library to cabal. We now support nix sandboxing.

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, alex, array, base, containers, directory
+      , happy, lens, mtl, stdenv, utf8-string, cabal-install
+      }:
+      mkDerivation {
+        pname = "angler";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          array base containers directory lens mtl utf8-string
+        ];
+        executableToolDepends = [ alex happy cabal-install ];
+        homepage = "https://github.com/angler-lang";
+        description = "Angler Functional Programming Language";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
