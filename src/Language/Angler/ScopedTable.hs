3a216fbcd483afc0bb660d0183bbacffec3d46f3
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-10-08 00:54:55 -0430

    Added a toList function that shows the *reachable* symbols

--- src/Language/Angler/ScopedTable.hs
+++ src/Language/Angler/ScopedTable.hs
@@ -10,6 +10,8 @@ module Language.Angler.ScopedTable
         -- scope handling
         , enterScope, exitScope
 
+        , toList
+
         -- on keys
         , mapKeys
         , filterByKey
@@ -43,7 +45,7 @@ exitScope :: ScopedTable sym -> ScopedTable sym
 exitScope = over tab_stack tail
 
 lookup :: String -> ScopedTable sym -> Maybe sym
-lookup str = msum . fmap (Map.lookup str) . view tab_stack
+lookup str = views tab_stack (msum . fmap (Map.lookup str))
 
 (!) :: ScopedTable sym -> String -> sym
 (!) tab = maybe message id . flip lookup tab
@@ -51,7 +53,10 @@ lookup str = msum . fmap (Map.lookup str) . view tab_stack
         message = error "SymbolTable.!: given key is not an element in the symbol table"
 
 elem :: String -> ScopedTable sym -> Bool
-elem str = any (P.elem str . Map.keys) . view tab_stack
+elem str = views tab_stack (any (P.elem str . Map.keys))
+
+elemInTop :: String -> ScopedTable sym -> Bool
+elemInTop str = views tab_stack (P.elem str . Map.keys . head)
 
 safeInsert :: String -> sym -> ScopedTable sym -> Either Error (ScopedTable sym)
 safeInsert str sym tab = if elem str tab
@@ -59,9 +64,15 @@ safeInsert str sym tab = if elem str tab
         else Right (over (tab_stack._head) (Map.insert str sym) tab)
 
 insert :: String -> sym -> ScopedTable sym -> ScopedTable sym
-insert str sym tab = if elem str tab
+insert str sym tab = if elemInTop str tab
         then (error . show . CheckError . AlreadyInSymbolTable) str
         else over (tab_stack._head) (Map.insert str sym) tab
+
+toList :: ScopedTable sym -> [(String, sym)]
+toList = views tab_stack (proccess . concatMap Map.toList)
+    where
+        proccess :: [(String, sym)] -> [(String, sym)]
+        proccess = Map.toList . foldr (uncurry Map.insert) Map.empty
 
 mapKeys :: (String -> String) -> ScopedTable sym -> ScopedTable sym
 mapKeys f = over (tab_stack.traverse) (Map.mapKeys f)
