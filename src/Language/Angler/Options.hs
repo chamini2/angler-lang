92806c86d9ac050cf3bbcdeaa081bd9ca7394b3d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-10-09 22:44:28 -0430

    Added --verbose and --mixfix-ast options

--- src/Language/Angler/Options.hs
+++ src/Language/Angler/Options.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Language.Angler.Options
-        ( Options(..), def
-        , optionDescrs
-        -- Lenses
-        , opt_warnings, opt_tokens, opt_ast, opt_stdin, opt_stdlib, opt_path
-        ) where
+module Language.Angler.Options where
 
 import           Control.Lens
 
@@ -19,8 +14,10 @@ import           System.Directory              (doesDirectoryExist)
 data Options
   = Options
         { _opt_warnings :: Bool
-        , _opt_tokens   :: Bool                 -- maybe change to _opt_verbose
+        , _opt_verbose  :: Bool
+        , _opt_tokens   :: Bool
         , _opt_ast      :: Bool
+        , _opt_mixfix   :: Bool
         , _opt_stdin    :: Bool
         -- , _opt_symbols  :: Bool
         -- , _opt_execute  :: Bool
@@ -36,8 +33,10 @@ makeLenses ''Options
 instance Default Options where
     def = Options
         { _opt_warnings = True
+        , _opt_verbose  = False
         , _opt_tokens   = False
         , _opt_ast      = False
+        , _opt_mixfix   = False
         , _opt_stdin    = False
         -- , _opt_symbols  = False
         -- , _opt_execute  = True
@@ -47,40 +46,53 @@ instance Default Options where
 
 optionDescrs :: [OptDescr (Options -> IO Options)]
 optionDescrs =
-        [ Option ['h'] ["help"]        ((NoArg . const . putLnSuccess) help)
+        [ Option ['h'] ["help"]          ((NoArg . const . putLnSuccess) help)
                 "shows this help message"
 
-        , Option ['v'] ["version"]     ((NoArg . const . putLnSuccess) version)
+        , Option ['v'] ["version"]       ((NoArg . const . putLnSuccess) version)
                 "shows version number"
 
-        , Option ['w'] ["warnings"]    (NoArg (optBool opt_warnings True))
+        , Option ['w'] ["warnings"]      (NoArg (optBool opt_warnings True))
                 "shows all warnings (default)"
 
-        , Option ['W'] ["no-warnings"] (NoArg (optBool opt_warnings False))
+        , Option ['W'] ["no-warnings"]   (NoArg (optBool opt_warnings False))
                 "suppress all warnings"
 
-        , Option ['s'] ["stdin"]      (NoArg (optBool opt_stdin True))
+        , Option ['s'] ["stdin"]         (NoArg (optBool opt_stdin True))
                 "shows the tokens recognized by the lexer"
 
-        , Option []    ["tokens"]      (NoArg (optBool opt_tokens True))
+        , Option ['V'] ["verbose"]       (NoArg (optBool opt_verbose True))
+                ("to show output for the several stages of interpreting, " ++
+                "this turns on the following flags: tokens, ast, mixfix-ast")
+
+        , Option []    ["no-verbose"]    (NoArg (optBool opt_verbose False))
+                "avoids showing output for the several stages of interpreting (default)"
+
+        , Option []    ["tokens"]        (NoArg (optBool opt_tokens True))
                 "shows the tokens recognized by the lexer"
 
-        , Option []    ["no-tokens"]   (NoArg (optBool opt_tokens False))
+        , Option []    ["no-tokens"]     (NoArg (optBool opt_tokens False))
                 "avoids showing the tokens recognized by the lexer (default)"
 
-        , Option []    ["ast"]         (NoArg (optBool opt_ast True))
-                "shows the AST parsed"
+        , Option []    ["ast"]           (NoArg (optBool opt_ast True))
+                "shows the parsed AST"
 
-        , Option []    ["no-ast"]      (NoArg (optBool opt_ast False))
-                "avoids showing the AST parsed (default)"
+        , Option []    ["no-ast"]        (NoArg (optBool opt_ast False))
+                "avoids showing the parsed AST (default)"
 
-        , Option []    ["stdlib"]      (NoArg (optBool opt_stdlib True))
+        , Option []    ["mixfix-ast"]    (NoArg (optBool opt_mixfix True))
+                "shows the parsed AST after passing the mixfix parser"
+
+        , Option []    ["no-mixfix-ast"] (NoArg (optBool opt_mixfix False))
+                "avoids showing the parsed AST after passing the mixfix parser (default)"
+
+        , Option []    ["stdlib"]        (NoArg (optBool opt_stdlib True))
                 "adds the standard library to the path (default)"
 
-        , Option []    ["no-stdlib"]   (NoArg (optBool opt_stdlib False))
+        , Option []    ["no-stdlib"]     (NoArg (optBool opt_stdlib False))
                 "removes the standard library from the path"
 
-        , Option ['p'] ["path"]        (ReqArg optPath "PATH")
+        , Option ['p'] ["path"]          (ReqArg optPath "PATH")
                 "adds a directory to the path"
         ]
     where
