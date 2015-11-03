b615b1f784bd0390b227115b262db228fede1f25
Matteo Ferrando <matteo.ferrando2@gmail.com>
2015-11-03 03:26:45 -0430

    Finished the MixfixParser, but it's way too slow now.

--- src/Language/Angler/MixfixParser.hs
+++ src/Language/Angler/MixfixParser.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
 
-module Language.Angler.MixfixParser where
+module Language.Angler.MixfixParser
+        ( parseMixfix ) where
 
 import           Language.Angler.AST
 import           Language.Angler.Error       hiding (ParseError)
@@ -17,27 +20,32 @@ import           Control.Applicative         (Alternative(..))
 
 import           Control.Lens                hiding (op, below, parts)
 
+import           Control.Monad               (when)
 import           Control.Monad.Except        (Except, runExcept)
 import           Control.Monad.State         (StateT, runStateT, gets)
 import           Control.Monad.Trans         (lift)
 
 import           Data.Function               (on)
+import           Data.Default                (Default(..))
 
 import           Data.Foldable               (toList)
 import           Data.List                   (sortBy)
 import           Data.Sequence               (Seq, fromList)
 
-import           Data.Maybe                  (isNothing)
+import           Data.Maybe                  (isJust, isNothing)
 
 import qualified Data.Map.Strict             as Map
 
 import           Text.Megaparsec             (ParsecT, runParserT, (<|>),
                                               choice, eof, many, token, try, some)
-import           Text.Megaparsec.Error       (ParseError, Message(..))
-import           Text.Megaparsec.Pos         (SourcePos, newPos)
+import           Text.Megaparsec.Error       (ParseError, Message(..),
+                                              errorMessages, errorPos)
+import           Text.Megaparsec.Pos         (SourcePos(..), newPos)
 import           Text.Megaparsec.Prim        (MonadParsec, getInput, getPosition,
                                               setInput, setPosition)
 import           Text.Megaparsec.ShowToken   (ShowToken(..))
+
+import           Debug.Trace
 
 --------------------------------------------------------------------------------
 -- Operator handling
@@ -83,6 +91,97 @@ op_prts op_fn op = wrap <$> views op_idn (op_fn . strOp) op
 type LoadPrecTableState = ScopedTable Operator
 type LoadPrecTable = StateT LoadPrecTableState (Except [Located Error])
 
+instance STScopedTable LoadPrecTableState Operator where
+        st_table = id
+
+instance Default LoadPrecTableState where
+        def = ST.empty
+
+runMixfixParser :: LoadPrecTable a -> Either [Located Error] (a, LoadPrecTableState)
+runMixfixParser = runExcept . flip runStateT def
+
+parseMixfix :: ModuleSpan -> Either [Located Error] ModuleSpan
+parseMixfix = over _Right fst . runMixfixParser . mixfixModule
+
+----------------------------------------
+
+mixfixModule :: ModuleSpan -> LoadPrecTable ModuleSpan
+mixfixModule = mod_body %%~ mixfixBody
+
+mixfixBody :: BodySpan -> LoadPrecTable BodySpan
+mixfixBody = mapM mixfixBodyStmt
+    where
+        mixfixBodyStmt :: BodyStmtSpan -> LoadPrecTable BodyStmtSpan
+        mixfixBodyStmt stmt = case stmt of
+                OpenType {}     -> stmt & open_type %%~ mixfixExprWhere
+                                      >>= open_cnts._Just %%~ mapM mixfixTypeBind
+                ReopenType {}   -> stmt & rpen_cnts %%~ mapM mixfixTypeBind
+                ClosedType {}   -> stmt & clsd_type %%~ mixfixExprWhere
+                                      >>= clsd_cnts %%~ mapM mixfixTypeBind
+                FunctionDecl {} -> stmt & fdec_type %%~ mixfixExprWhere
+                FunctionDef {}  -> stmt & fdef_args %%~ mixfixArgument
+                                      >>= fdef_expr mixfixExprWhere
+
+                OperatorDef idn fix spn -> do
+                        let idn' = view idn_str idn
+                            fix' = set fix_annot () fix
+                        merr <- safeInsertSc idn' (Operator idn' fix')
+                        when (isJust merr) $ do
+                                let Just err = merr
+                                throwError [Loc spn err]
+                        return stmt
+
+mixfixExprWhere :: ExprWhereSpan -> LoadPrecTable ExprWhereSpan
+mixfixExprWhere whre = whre & whre_insd %%~ mixfixExpression
+                          >>= whre_body._Just %%~ mixfixBody
+
+mixfixExpression :: ExpressionSpan -> LoadPrecTable ExpressionSpan
+mixfixExpression expr = do
+        eith <- runOpParser generateOpParser (view exp_annot expr) [expr]
+        flattenExpression <$> case eith of
+                Left perr -> do
+                        let msgs = errorMessages perr
+                            spn  = posSpan (errorPos perr)
+                        throwError (fmap (Loc spn . messageError) msgs)
+                Right x -> return x
+    where
+        posSpan :: SourcePos -> SrcSpan
+        posSpan pos = SrcSpanPoint (sourceName pos) (sourceLine pos) (sourceColumn pos)
+
+        flattenExpression :: ExpressionSpan -> ExpressionSpan
+        flattenExpression expr' = case expr' of
+                Apply xs an -> case toList xs of
+                        [x] -> flattenExpression x
+                        _   -> Apply (fmap flattenExpression xs) an
+                _ -> expr'
+
+        messageError :: Message -> Error
+        messageError msg = CheckError $ case msg of
+                Expected   str -> CErrExpected str
+                Unexpected str -> CErrUnexpected str
+                Message    str -> CErr str
+
+mixfixTypeBind :: TypeBindSpan -> LoadPrecTable TypeBindSpan
+mixfixTypeBind = typ_type %%~ mixfixExprWhere
+
+mixfixArgument :: ArgumentSpan -> LoadPrecTable ArgumentSpan
+mixfixArgument = (exprArg <$>) . mixfixExpression . argExpr
+    where
+        argExpr :: ArgumentSpan -> ExpressionSpan
+        argExpr arg = case arg of
+                DontCare an        -> Var "_" an
+                VarBinding idn an  -> Var idn an
+                ApplyBinding xs an -> Apply (fmap argExpr xs) an
+        exprArg :: ExpressionSpan -> ArgumentSpan
+        exprArg expr = case expr of
+                Var "_" an  -> DontCare an
+                Var idn an  -> VarBinding idn an
+                Apply xs an -> ApplyBinding (fmap exprArg xs) an
+                _           -> error "MixfixParser.mixfixArgument: impossible case"
+
+mixfixImplicit :: ImplicitBindingSpan -> LoadPrecTable ImplicitBindingSpan
+mixfixImplicit = impl_expr %%~ mixfixExpression
+
 --------------------------------------------------------------------------------
 -- Expressions parser
 
@@ -108,8 +207,8 @@ mapTryChoice :: (Foldable f, Functor f, MonadParsec s m t) => f a -> (a -> m b)-
 mapTryChoice ops act = choice (fmap (try . act) ops)
 
 type ConSnoc f a = (Alternative f, Cons (f a) (f a) a a, Snoc (f a) (f a) a a)
-listSpan :: ConSnoc f ExprSpan => f ExprSpan -> SrcSpan
-listSpan xs = srcSpanSpan (xs^?!_head.exp_annot) (xs^?!_last.exp_annot)
+expListSpan :: ConSnoc f ExprSpan => f ExprSpan -> SrcSpan
+expListSpan xs = srcSpanSpan (xs^?!_head.exp_annot) (xs^?!_last.exp_annot)
 
 precTable :: OpParser PrecTable
 precTable = gets (buildTable . sort . fmap snd . ST.toList)
@@ -217,15 +316,25 @@ var str = satisfy [Expected str] sameVar
 generateOpParser :: OpParser ExprSpan
 generateOpParser = topP <* eof
     where
-        closedPartP :: ConSnoc f ExprSpan => OperatorRepr -> OpParser (f ExprSpan)
-        closedPartP = foldr go (pure empty) . closedPart
+        closedPartP :: ConSnoc f ExprSpan => OperatorRepr -> OpParser (SrcSpan, f ExprSpan)
+        closedPartP = foldr go (pure (SrcSpanNoInfo, empty)) . closedPart
             where
-                go :: Alternative f => OperatorPart -> OpParser (f ExprSpan) -> OpParser (f ExprSpan)
-                go mpart act = partP mpart <|> act
-                partP :: Alternative f => OperatorPart -> OpParser (f ExprSpan)
+                go :: Alternative f => OperatorPart
+                                    -> OpParser (SrcSpan, f ExprSpan)
+                                    -> OpParser (SrcSpan, f ExprSpan)
+                go mpart act = do
+                        (xsp, mx) <- partP mpart
+                        (asp, xs) <- act
+                        return (srcSpanSpan xsp asp, mx <|> xs)
+                -- go mpart act = partP mpart <|> act
+                partP :: Alternative f => OperatorPart -> OpParser (SrcSpan, f ExprSpan)
                 partP mprt = case mprt of
-                        Just prt -> var prt *> pure empty
-                        Nothing  -> pure <$> topP
+                        Just prt -> do
+                                x <- var prt
+                                return (x^.exp_annot, empty)
+                        Nothing  -> do
+                                x <- topP
+                                return (x^.exp_annot, pure x)
                 closedPart :: OperatorRepr -> OperatorRepr
                 closedPart = reverse . clean . reverse . clean
                     where
@@ -243,7 +352,7 @@ generateOpParser = topP <* eof
         bottomP :: OpParser ExprSpan
         bottomP = do
                 xs <- fromList <$> (some basicToken)
-                return (Apply xs (listSpan xs))
+                return (Apply xs (expListSpan xs))
             where
                 -- This tries to get a basic token, if we couldn't get even one,
                 -- it gets any token, and then continues with the basic tokens,
@@ -267,132 +376,93 @@ generateOpParser = topP <* eof
                                         _          -> True
                                     where
                                         opParts :: [String]
-                                        opParts = error "opParts: use prec variable"
+                                        opParts = toListOf (each.each.each.each._Just) prec
 
                         closedfixP :: OpParser ExprSpan
                         closedfixP = do
                                 closedOps <- concatMap (levelFixity (Closedfix ())) <$> precTable
                                 mapTryChoice closedOps $ \op -> do
-                                        xs <- closedPartP op
-                                        let spn = listSpan xs
-                                            nam = opStr op
+                                        (spn, xs) <- closedPartP op
+                                        let nam = opStr op
                                         return (Apply (Var nam spn <| xs) spn)
 
         levelP :: PrecLevel -> [OpParser ExprSpan] -> OpParser ExprSpan
-        levelP lvl below = try nonAssocP <|> try rightP <|> leftP
+        levelP lvl hole = try nonAssocP <|> try rightP <|> leftP
             where
-                belowP :: OpParser ExprSpan
-                belowP = choice below
+                holeP :: OpParser ExprSpan
+                holeP = choice hole
 
                 infixOps :: Associativity -> [OperatorRepr]
                 infixOps ass = levelFixity (Infix ass dummy ()) lvl
                     where
                         dummy = error "infixOps: shouldn't be evaluated"
 
-                genericOpP :: ConSnoc f ExprSpan
-                           => [OperatorRepr]    -- the operators to use
-                           -> Bool              -- if there is a left hand side
-                           -> Bool              -- if tehre is a right hand side
-                           -> OpParser (ExprSpan, f ExprSpan)
-                genericOpP list leftHS rightHS = mapTryChoice list $ \op -> do
-                        leftx  <- if leftHS  then pure <$> belowP else empty
-                        clxs   <- closedPartP op
-                        rightx <- if rightHS then pure <$> belowP else empty
-
-                        let xs  = leftx <|> clxs <|> rightx
-                            spn = listSpan xs
-                            nam = opStr op
-
-                        return (Var nam spn, xs)
-
                 nonAssocP :: OpParser ExprSpan
-                nonAssocP = do
-                        (op, xs) <- genericOpP (infixOps NonAssoc) True True
-                        return (Apply (op <| xs) (listSpan xs))
-                -- nonAssocP :: OpParser ExprSpan
-                -- nonAssocP = mapTryChoice (infixOps NonAssoc) $ \op -> do
-                --         leftHS  <- belowP
-                --         clxs    <- closedPartP op
-                --         rightHS <- belowP
-                --         let spn = (srcSpanSpan `on` (view exp_annot)) leftHS rightHS
-                --             nam = opStr op
-                --             xs  = leftHS <| (clxs |> rightHS)
-                --         return (Apply (Var nam spn <| xs) spn)
+                nonAssocP = mapTryChoice (infixOps NonAssoc) $ \op -> do
+                        leftHS    <- holeP
+                        (_, clxs) <- closedPartP op
+                        rightHS   <- holeP
+                        let spn = (srcSpanSpan `on` (view exp_annot)) leftHS rightHS
+                            nam = opStr op
+                            xs  = leftHS <| (clxs |> rightHS)
+                        return (Apply (Var nam spn <| xs) spn)
 
                 rightP :: OpParser ExprSpan
                 rightP = do
                         xs <- some (try prefixP <|> rightAssocP)
-                        x  <- belowP
+                        x  <- holeP
                         return (foldr go x xs)
                     where
                         go :: (ExprSpan, Seq ExprSpan) -> ExprSpan -> ExprSpan
                         go (op, xs) x = let xs' = xs |> x
-                                        in Apply (op <| xs') (listSpan xs')
+                                        in Apply (op <| xs') (expListSpan xs')
 
                         prefixP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        prefixP = genericOpP prefixOps False False
+                        prefixP = mapTryChoice prefixOps $ \op -> do
+                                (spn, xs) <- closedPartP op
+                                let nam = opStr op
+                                return (Var nam spn, xs)
                             where
                                 prefixOps :: [OperatorRepr]
                                 prefixOps = levelFixity (Prefix dummy ()) lvl
                                     where
                                         dummy = error "prefixOps: shouldn't be evaluated"
-                        -- prefixP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        -- prefixP = mapTryChoice prefixOps $ \op -> do
-                        --         xs <- closedPartP op
-                        --         let spn = listSpan xs
-                        --             nam = opStr op
-                        --         return (Var nam spn, xs)
 
                         rightAssocP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        rightAssocP = genericOpP (infixOps RightAssoc) True False
-                        -- rightAssocP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        -- rightAssocP = mapTryChoice (infixOps RightAssoc) $ \op -> do
-                        --         leftHS <- belowP
-                        --         clxs   <- closedPartP op
-                        --         let xs  = leftHS <| clxs
-                        --             spn = listSpan xs
-                        --             nam = opStr op
-                        --         return (Var nam spn, xs)
+                        rightAssocP = mapTryChoice (infixOps RightAssoc) $ \op -> do
+                                leftHS      <- holeP
+                                (clsp,clxs) <- closedPartP op
+                                let xs  = leftHS <| clxs
+                                    spn = srcSpanSpan (leftHS^.exp_annot) clsp
+                                    nam = opStr op
+                                return (Var nam spn, xs)
 
                 leftP :: OpParser ExprSpan
                 leftP = do
-                        x  <- belowP
+                        x  <- holeP
                         xs <- some (try postfixP <|> leftAssocP)
                         return (foldl go x xs)
                     where
                         go :: ExprSpan -> (ExprSpan, Seq ExprSpan) -> ExprSpan
                         go x (op, xs) = let xs' = x <| xs
-                                        in Apply (op <| xs') (listSpan xs')
+                                        in Apply (op <| xs') (expListSpan xs')
 
                         postfixP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        postfixP = genericOpP postfixOps False False
+                        postfixP = mapTryChoice postfixOps $ \op -> do
+                                (spn, xs) <- closedPartP op
+                                let nam = opStr op
+                                return (Var nam spn, xs)
                             where
                                 postfixOps :: [OperatorRepr]
                                 postfixOps = levelFixity (Postfix dummy ()) lvl
                                     where
                                         dummy = error "postfixOps: shouldn't be evaluated"
-                        -- postfixP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        -- postfixP = mapTryChoice postfixOps $ \op -> do
-                        --         xs <- closedPartP op
-                        --         let spn = listSpan xs
-                        --             nam = opStr op
-                        --         return (Var nam spn, xs)
 
                         leftAssocP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        leftAssocP = genericOpP (infixOps LeftAssoc) False True
-                        -- leftAssocP :: ConSnoc f ExprSpan => OpParser (ExprSpan, f ExprSpan)
-                        -- leftAssocP = mapTryChoice (infixOps LeftAssoc) $ \op -> do
-                        --         clxs    <- closedPartP op
-                        --         rightHS <- belowP
-                        --         let xs  = clxs |> rightHS
-                        --             spn = listSpan xs
-                        --             nam = opStr op
-                        --         return (Var nam spn, xs)
-
-
-
-mixfixArgument = undefined
-mixfixBody = undefined
-mixfixExpression = undefined
-mixfixImplicit = undefined
-mixfixTypeBind = undefined
+                        leftAssocP = mapTryChoice (infixOps LeftAssoc) $ \op -> do
+                                (clsp, clxs) <- closedPartP op
+                                rightHS      <- holeP
+                                let xs  = clxs |> rightHS
+                                    spn = srcSpanSpan clsp (rightHS^.exp_annot)
+                                    nam = opStr op
+                                return (Var nam spn, xs)
